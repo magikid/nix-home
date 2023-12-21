3bfeb26bb0bfc37ba14f8a64599984beea5a42b8
Chris W Jones <chris@christopherjones.us>
2023-12-21 14:40:58 -0500

    feat: composer uses my php now

--- laptop.nix
+++ laptop.nix
@@ -1,19 +1,24 @@
 { config, pkgs, lib, ... }:
-{
-  home.packages = [
-    pkgs.jetbrains.phpstorm
-    pkgs.jetbrains.pycharm-professional
-    (pkgs.php83.buildEnv {
+let
+  myPhp = pkgs.php82.buildEnv {
       extensions = ({ enabled, all}: enabled ++ (with all; [
         amqp
+        apcu
         imagick
         xdebug
       ]));
       extraConfig = ''
         xdebug.mode=debug
+        memory_limit=512M
       '';
-    })
-    pkgs.php83Packages.composer
+    };
+in
+{
+  home.packages = [
+    pkgs.jetbrains.phpstorm
+    pkgs.jetbrains.pycharm-professional
+    (pkgs.php82Packages.composer.override {php = myPhp;})
+    myPhp
 
     (pkgs.writeShellScriptBin "phpstorm-url-handler"
       (builtins.readFile bin/phpstorm-url-handler.sh))
