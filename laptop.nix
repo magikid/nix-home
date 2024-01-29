d46aa4f9741faa5f84fe0b69b016d97a963cbf08
Chris W Jones <chris@christopherjones.us>
2024-01-29 10:53:34 -0500

    Get slack to play nicely with wayland

--- laptop.nix
+++ laptop.nix
@@ -12,17 +12,28 @@ let
         memory_limit=512M
       '';
     };
+  slack = pkgs.slack.overrideAttrs (old: {
+    installPhase = old.installPhase + ''
+      rm $out/bin/slack
+
+      makeWrapper $out/lib/slack/slack $out/bin/slack \
+        --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+        --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
+        --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+    '';
+  });
 in
 {
   home.packages = [
     pkgs.ansible
+    pkgs.ddosify
     pkgs.jetbrains.phpstorm
     pkgs.jetbrains.pycharm-professional
     pkgs.kcachegrind
     (pkgs.php82Packages.composer.override {php = myPhp;})
     myPhp
     pkgs.nixos-rebuild
-    pkgs.slack
+    slack
     pkgs.vlc
     pkgs.vscode
 
