f3645f3f236adead45dffcb73d528156cfcb7459
Chris W Jones <chris@christopherjones.us>
2024-03-12 11:28:31 -0400

    Use 23.11 instead of unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of chrisj";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/23.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
