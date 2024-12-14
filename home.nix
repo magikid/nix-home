ed6bfd93065f5eb1aa53f6d165f9968b01358431
Chris W Jones <chris@christopherjones.us>
2024-12-13 23:37:39 -0500

    fish: no linking vars

--- home.nix
+++ home.nix
@@ -115,7 +115,6 @@ in
     "fish/functions/archive-folder.fish".text = (builtins.readFile apps/fish/archive-folder.fish);
     "fish/functions/ensure_tmux_is_running.fish".text = (builtins.readFile apps/fish/ensure_tmux_is_running.fish);
     "fish/completions/nix.fish".source = "${pkgs.nix}/share/fish/vendor_completions.d/nix.fish";
-    "fish/fish_variables".text = (builtins.readFile apps/fish/fish_variables);
   };
 
   programs.fish.shellInit = ''
