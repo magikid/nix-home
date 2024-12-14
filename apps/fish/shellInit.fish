91c5c0e19c4811daae3cb415e64c2245193b40a2
Chris W Jones <chris@christopherjones.us>
2024-12-13 23:21:25 -0500

    fish: add configure command

--- apps/fish/shellInit.fish
+++ apps/fish/shellInit.fish
@@ -1,4 +1,5 @@
 set fish_greeting # Disable greeting
+tide configure --auto --style=Classic --prompt_colors='True color' --classic_prompt_color=Dark --show_time=No --classic_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=Yes --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
 set tide_right_prompt_items \
     status \
     cmd_duration \
