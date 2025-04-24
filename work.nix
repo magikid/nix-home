5c19b0f6a53ed536ffb147c0154c05d839dfc6de
Chris W Jones <cjones@vultr.com>
2025-04-24 11:38:06 -0400

    work: got jrnl setup again

--- work.nix
+++ work.nix
@@ -41,6 +41,7 @@ in
     pkgs.jetbrains.phpstorm
     pkgs.jetbrains.pycharm-community
     pkgsUnstable.jjui
+    pkgs.jrnl
     pkgs.k6
     pkgs.kubernetes-helm
     pkgs.kind
@@ -86,4 +87,5 @@ in
   home.sessionVariables.NIX_SSL_CERT_FILE = "$XDG_CONFIG_HOME/constant/vultr_cacert.pem";
   programs.git.userEmail = lib.mkForce "cjones@vultr.com";
   programs.jujutsu.settings.user.email = lib.mkForce "cjones@vultr.com";
+  xdg.configFile."jrnl/jrnl.yaml".text = (builtins.readFile text/jrnl.yaml);
 }
