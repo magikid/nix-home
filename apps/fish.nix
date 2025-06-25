1ebe0a35e8393624843da4aa259d75961ee3ea02
Chris W Jones <cjones@vultr.com>
2025-06-25 13:09:28 -0400

    fish: add c,p,dr abbreviations

--- apps/fish.nix
+++ apps/fish.nix
@@ -9,10 +9,15 @@
       ".." = "cd ..";
       "..." = "cd ../..";
       "...." = "cd ../../..";
+      c = "pbcopy";
       dcdn = "docker compose down";
       dce = "docker compose exec";
       dcps = "docker compose ps";
       dcupd = "docker compose up -d";
+      dr = {
+        expansion = "docker run --rm -it -v $PWD:/app -v $HOME/ca_certs:/ca_certs -w /app % sh";
+        setCursor = true;
+      };
       df = "df -hl";
       dirsize = "du -cxh -d 1 | sort -h";
       got = "git";
@@ -30,6 +35,7 @@
       reload = "source $XDG_CONFIG_HOME/fish/config.fish";
       tp = "teleport-ssh";
       tpi = "teleport-ssh --internal";
+      v = "pbpaste";
     };
     programs.fish.shellInit = (builtins.readFile fish/shellInit.fish);
 }
