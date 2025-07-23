e64017787acc6e70db3ac1b12ec4f7c858e7f27e
Chris W Jones <cjones@vultr.com>
2025-07-23 12:25:18 -0400

    ssh: import local-only config

--- apps/ssh.nix
+++ apps/ssh.nix
@@ -7,6 +7,9 @@
         extraConfig = ''
             TCPKeepAlive yes
         '';
+        includes = [
+            "~/.ssh/private-config"
+        ];
         matchBlocks = {
             "*" = {
                 extraOptions = {
