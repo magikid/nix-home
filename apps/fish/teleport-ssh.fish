a5c784b89ab3257c1091f58c590cb5d0c2375bbe
Chris W Jones <cjones@vultr.com>
2025-10-20 14:55:07 -0400

    fish: add username option to teleport-ssh

--- apps/fish/teleport-ssh.fish
+++ apps/fish/teleport-ssh.fish
@@ -1,11 +1,17 @@
 function teleport-ssh
-    argparse 'h/help' 'i/internal' -- $argv
+    argparse 'h/help' 'i/internal' 'r/root' -- $argv
     or return
 
     if set -ql _flag_help
-        echo "teleport-ssh [-h|--help] [-i|--internal] <host> [<command>...]" >&2
+        echo "teleport-ssh [-h|--help] [-i|--internal] [-r|--root] <host> [<command>...]" >&2
         return 1
     end
+
+    set -l username cjones
+    if set -ql _flag_root
+        set username root
+    end
+
 
     set -l host $argv[1]
     set -l commands $argv[2..-1]
@@ -18,9 +24,9 @@ function teleport-ssh
         set clusterFlag "--cluster"
         set cluster tp-internal.constant.com
         echo "Using internal cluster: $cluster"
-        tsh ssh --cluster tp-internal.constant.com root@$host $commands
+        tsh ssh --cluster tp-internal.constant.com $username@$host $commands
         return $status
     end
 
-    tsh ssh root@$host $commands
+    tsh ssh $username@$host $commands
 end
