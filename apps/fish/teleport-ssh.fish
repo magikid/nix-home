b753fa71253130125097c1a28009c168e9a853f3
Chris W Jones <cjones@vultr.com>
2025-12-08 13:05:15 -0500

    fish: add verbose flag to teleport-ssh

--- apps/fish/teleport-ssh.fish
+++ apps/fish/teleport-ssh.fish
@@ -1,17 +1,25 @@
 function teleport-ssh
-    argparse 'h/help' 'i/internal' 'r/root' -- $argv
+    argparse 'h/help' 'i/internal' 'r/root' 'v/verbose' -- $argv
     or return
 
     if set -ql _flag_help
-        echo "teleport-ssh [-h|--help] [-i|--internal] [-r|--root] <host> [<command>...]" >&2
+        echo "teleport-ssh [-h|--help] [-i|--internal] [-r|--root] [-v|--verbose] <host> [<command>...]" >&2
         return 1
+    end
+
+    set -l verbose false
+    if set -ql _flag_verbose
+        set verbose true
+        echo "Verbose mode enabled"
     end
 
     set -l username cjones
     if set -ql _flag_root
         set username root
     end
-
+    if test $verbose = true
+        echo "Using username: $username"
+    end
 
     set -l host $argv[1]
     set -l commands $argv[2..-1]
@@ -19,14 +27,28 @@ function teleport-ssh
         echo "No host provided, aborting" >&2
         return 1
     end
+    if test $verbose = true
+        echo "Connecting to host: $host"
+        if test (count $commands) -gt 0
+            echo "With commands: $commands"
+        else
+            echo "No commands provided, starting interactive session"
+        end
+    end
 
     if set -ql _flag_internal
         set clusterFlag "--cluster"
         set cluster tp-internal.constant.com
         echo "Using internal cluster: $cluster"
+        if test $verbose = true
+            echo "Running tsh ssh --cluster $cluster $username@$host $commands"
+        end
         tsh ssh --cluster tp-internal.constant.com $username@$host $commands
         return $status
     end
 
+    if test $verbose = true
+        echo "Running tsh ssh $username@$host $commands"
+    end
     tsh ssh $username@$host $commands
 end
